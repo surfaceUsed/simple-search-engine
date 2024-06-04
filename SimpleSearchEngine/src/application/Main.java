ca0154593ee6b0b74c0350183d5f997576f8db3f
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-06-04 18:51:17 +0200

    Update Main.java

--- SimpleSearchEngine/src/application/Main.java
+++ SimpleSearchEngine/src/application/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Takes path to file "src/application/data/data.txt" as command line argument. 
         new Controller(args[0]).run();
     }
 }
